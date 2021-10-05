23c54d3be03cee74c22a211d351d1959595b5db1
rajeevkghosh <54052253+rajeevkghosh@users.noreply.github.com>
2021-10-05 14:42:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ provider "google" {
 }
 
 
-resource "google_pubsub_topic" "example2" {
-  name = "example-topic-2"
+resource "google_pubsub_topic" "example3" {
+  name = "example-topic-3"
 
   kms_key_name = "projects/tokyo-nomad-323506/locations/us-central1/keyRings/keyring-example-pubsub/cryptoKeys/crypto-key-example-pubsub"
 
